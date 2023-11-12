dd24738b4da5ced7e65297a64fa4210c727e6114
Francesco <francescoconvertino.ita@gmail.com>
2023-11-12 19:42:32 +0100

    test

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return "Hello, World!"
 
+@app.route('/test')
+def hello():
+    return "Helloooo, World!"
 if __name__ == '__main__':
     # Only run the app if executed directly, not as an import
     port = int(os.environ.get('PORT', 5000))
